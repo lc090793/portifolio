f91b6846e26b96b39d91cd2d09ce0b86c727b818
Leonardo <lc08093@gmail.com>
2023-07-21 16:11:51 -0300

    21/07/2023 6º

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { Lenis as ReactLenis } from "@studio-freight/react-lenis";
 import Banner from "@/components/Banner";
 import Bottom from "@/components/Bottom";
 import Skills from "@/components/Skills";
@@ -8,13 +6,13 @@ import Social from "@/components/Social";
 import Process from "@/components/Process";
 export default function Home() {
   return (
-    <ReactLenis root>
+    <>
       <Bottom/>
       <Banner />
       <Skills />
       <Works />
       <Social/>
       <Process/>
-    </ReactLenis>
+    </>
   );
 }
